af053bda674c8215d17229ceaa4d3d0a4b801ce0
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-10-27 18:28:27 -0400

    added seed to db

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,5 +2,10 @@
 
 INSERT INTO MeetingRoom (Name, Capacity, Floor) VALUES
 ('Meeting Room 1', 4, 10),
-('Meeting Room 2', 6, 20),
+('Conference Room A', 10, 20),
 ('Meeting Room 3', 8, 30);
+
+INSERT INTO Booking (RoomId, MeetingName, StartDateTime, EndDateTime, Attendees) VALUES
+(1, 'Team Meeting', '2023-10-28 09:00:00', '2023-10-28 10:00:00', ARRAY['janedoe@email.com', 'johnsmith@email.com'])
+(2, 'Cloud Security Meeting', '2023-10-28 14:00:00', '2023-10-28 16:00:00', ARRAY['bobross@email.com', 'sharronstone@email.com'])
+(3, 'Network Security Meeting', '2023-10-29 11:30:00', '2023-10-28 1:00:00', ARRAY['tonyhawk@email.com', 'erickoston@email.com'])
