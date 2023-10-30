21fe7e9a703ae358f8ebf33dea0f9e5d78f70d48
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-10-30 16:11:10 -0400

    bug fixes and ui improvements

--- front-end/src/components/MeetingRooms.js
+++ front-end/src/components/MeetingRooms.js
@@ -1,30 +1,61 @@
 import React from "react";
-// import axios from 'axios';
+import axios from 'axios';
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
 import "../styles/meetingroom-styles.css";
 import PeopleIcon from '@mui/icons-material/People';
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
 
+//importing environmental variable
+const API = process.env.REACT_APP_API_URL;
+
+console.log(API);
+
+
 const MeetingRooms = () => {
+  const [meetings, setMeetings] = useState([]);
+  useEffect(() => {
+    axios.get(`${API}/api/meeting-rooms`)
+      .then((res) => {
+          setMeetings(res.data);
+        },
+        (error) => console.log("get", error)
+      )
+      .catch((c) => console.warn("catch", c));
+  }, []);
+
+  console.log(meetings)
+
   return (
-    <div className="meeting-rooms-wrapper">
-      <div className="meeting-rooms-desc-container">
-        <b id="meeting-room-desc">
-          Meeting Room A
-        </b>
-      </div>
-      <div className="meeting-rooms-capacity-container">
-        <PeopleIcon/>
-        <b id="meeting-room-capacity">
-          Capacity:
-        </b>
-      </div>
-      <div className="meeting-rooms-floor-container">
-       <CorporateFareIcon/> 
-       <b id="meeting-room-floors">
-        Floor:
-       </b>
-      </div>
-    </div>
+          <>         
+          {meetings.map((meeting, id) => {
+            console.log(meeting)
+            console.log(meeting.roomid)
+            return(
+                <Link className="meeting-room-link" key={meeting.roomid} to={`/meeting-rooms/${meeting.roomid}`}>
+              <div className="meeting-rooms-wrapper">
+              <div className="meeting-rooms-desc-container">
+                <b id="meeting-room-desc">
+                  {meeting.name}
+                </b>
+              </div>
+              <div className="meeting-rooms-capacity-container">
+                <PeopleIcon/>
+                <b id="meeting-room-capacity">
+                  Capacity: {meeting.capacity}
+                </b>
+              </div>
+              <div className="meeting-rooms-floor-container">
+              <CorporateFareIcon/> 
+              <b id="meeting-room-floors">
+                Floor: {meeting.floor}
+              </b>
+              </div>
+              </div>
+                </Link>
+            )
+          })}
+          </>
   )
 };
 
