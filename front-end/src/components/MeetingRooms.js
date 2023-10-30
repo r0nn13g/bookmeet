23845d2b01874d6f599f4406e14ffe3064ec10ab
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-10-30 13:59:07 -0400

    added css grid for meeting rooms, ui improvements

--- front-end/src/components/MeetingRooms.js
+++ front-end/src/components/MeetingRooms.js
@@ -1,20 +1,28 @@
 import React from "react";
 // import axios from 'axios';
 import "../styles/meetingroom-styles.css";
+import PeopleIcon from '@mui/icons-material/People';
+import CorporateFareIcon from '@mui/icons-material/CorporateFare';
 
 const MeetingRooms = () => {
   return (
     <div className="meeting-rooms-wrapper">
       <div className="meeting-rooms-desc-container">
-        <b id="meeting-room-description">
+        <b id="meeting-room-desc">
           Meeting Room A
         </b>
       </div>
       <div className="meeting-rooms-capacity-container">
-        Capacity:
+        <PeopleIcon/>
+        <b id="meeting-room-capacity">
+          Capacity:
+        </b>
       </div>
       <div className="meeting-rooms-floor-container">
+       <CorporateFareIcon/> 
+       <b id="meeting-room-floors">
         Floor:
+       </b>
       </div>
     </div>
   )
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "../styles/home-styles.css";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import MeetingRooms from '../components/MeetingRooms.js';
-// import "../styles/video-styles.css";
+
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-        <b>
-         Home
-        </b>
+      <div className="button-group-container">
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button>Meeting Rooms</Button>
+          <Button>Bookings</Button>
+          <Button>New Room</Button>
+        </ButtonGroup>
+
+      </div>
       <div className="meeting-room-container">
         <MeetingRooms />
       </div>
