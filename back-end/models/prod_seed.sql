53639e09da70be690c6e39db7bdfccc6284287f0
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-10-31 00:50:31 -0400

    bug fixes

--- back-end/models/prod_seed.sql
+++ back-end/models/prod_seed.sql
@@ -7,4 +7,4 @@ INSERT INTO Booking (meetingRoomId, meetingName, startDateTime, endDateTime) VAL
 (1, 'Team Meeting', '2023-10-24T14:00', '2023-10-24T15:00'),
 (2, 'Cloud Security Meeting', '2023-10-25T13:00', '2023-10-25T14:00'), 
 (3, 'Network Security Meeting', '2023-10-26T14:00', '2023-10-26T15:00'),
-(4, 'Scrum Standup', '2023-10-27T12:00', '2023-10-27T13:00');
+(3, 'Scrum Standup', '2023-10-27T12:00', '2023-10-27T13:00');
