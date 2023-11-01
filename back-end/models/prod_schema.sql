33440510285382fa3e2620f27581774359f909bf
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-11-01 02:52:22 -0400

    models refactor

--- back-end/models/prod_schema.sql
+++ back-end/models/prod_schema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS MeetingRoom;
 DROP TABLE IF EXISTS Booking;
 
--- Create the MeetingRoom table
 CREATE TABLE MeetingRoom (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -9,11 +8,9 @@ CREATE TABLE MeetingRoom (
     floor INT NOT NULL
 );
 
-
--- Create the Booking table
 CREATE TABLE Booking (
     bookingId SERIAL PRIMARY KEY,
-    meetingRoomId INT REFERENCES MeetingRoom(id),
+    meetingRoomId INT REFERENCES MeetingRoom(id) NOT NULL,
     meetingName VARCHAR(255) NOT NULL,
     startDateTime TIMESTAMP WITH TIME ZONE NOT NULL,
     endDateTime TIMESTAMP WITH TIME ZONE NOT NULL
--- back-end/models/schema.sql
+++ back-end/models/schema.sql
@@ -3,21 +3,15 @@ CREATE DATABASE room_booking;
 
 \c room_booking;
 
--- DROP TABLE IF EXISTS MeetingRoom;
--- DROP TABLE IF EXISTS Booking;
-
--- Create the MeetingRoom table
 CREATE TABLE MeetingRoom (
-    id SERIAL PRIMARY KEY, -- Identity column for a unique ID
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     capacity INT NOT NULL,
     floor INT NOT NULL
 );
 
-
--- Create the Booking table
 CREATE TABLE Booking (
-    bookingId SERIAL PRIMARY KEY, -- Identity column for booking ID
+    bookingId SERIAL PRIMARY KEY, 
     meetingRoomId INT REFERENCES MeetingRoom(id) NOT NULL,
     meetingName VARCHAR(255) NOT NULL,
     startDateTime TIMESTAMP WITH TIME ZONE NOT NULL,
