81b06b6e150310c87b323435921f04cf146dfa17
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-11-01 21:00:25 -0400

    bug fixes and ui improvements

---
+++ front-end/src/pages/ShowMeetings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GetMeetings from "../components/GetMeetings";
+
+const ShowBookings = () => {
+  return (
+    <div>
+      <GetMeetings />
+    </div>
+  )
+};
+
+export default ShowBookings;
