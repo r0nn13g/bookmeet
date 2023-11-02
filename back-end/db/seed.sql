6d0e66a8d1f327538f19cba28f26ed4f0f68fbce
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-11-01 20:11:20 -0400

    bug fixes

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -6,7 +6,7 @@ INSERT INTO MeetingRoom (name, capacity, floor) VALUES
 ('Meeting Room 3', 8, 30);
 
 INSERT INTO Booking (meetingRoomId, meetingName, startDateTime, endDateTime) VALUES
-(1, 'Team Meeting', '2023-10-24T14:00', '2023-10-24T15:00'),
+(1, 'Blackstone Round 1', '2023-11-6T14:00', '2023-11-6T15:00'),
 (2, 'Cloud Security Meeting', '2023-10-25T13:00', '2023-10-25T14:00'), 
 (3, 'Network Security Meeting', '2023-10-26T14:00', '2023-10-26T15:00'),
 (3, 'Scrum Standup', '2023-10-27T12:00', '2023-10-27T13:00');
